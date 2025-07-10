7310790a2a5a1d33614e3ad64cd2b5dc22a5514a
Carles Escrig Royo <esroyo@gmail.com>
2025-07-10 23:51:14 +0200

    fix: add missing fonts + upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import deno from '@deno/astro-adapter';
 
 // https://astro.build/config
 export default defineConfig({
+    image: {
+        layout: 'full-width',
+        responsiveStyles: true,
+    },
     output: 'static',
     adapter: deno(),
 });
